30edd81966d91834e77a4cea4883e87be0fb1b2f
jerrybarona <jab2397@columbia.edu>
2019-04-17 02:41:14 -0700

    Added Material-UI dependencies

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createProject } from '../../store/actions/projectActions'
-import { Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
+import { Redirect } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 class CreateProject extends Component {
   state = {
-    title: '',
-    content: ''
-  }
-  handleChange = (e) => {
+    title: "",
+    content: ""
+  };
+  handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
-    })
-  }
-  handleSubmit = (e) => {
+    });
+  };
+  handleSubmit = e => {
     e.preventDefault();
     // console.log(this.state);
     this.props.createProject(this.state);
-    this.props.history.push('/');
-  }
+    this.props.history.push("/");
+  };
   render() {
     const { auth } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' /> 
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
           <h5 className="grey-text text-darken-3">Create a New Project</h5>
           <div className="input-field">
-            <input type="text" id='title' onChange={this.handleChange} />
+            <input type="text" id="title" onChange={this.handleChange} />
             <label htmlFor="title">Project Title</label>
           </div>
           <div className="input-field">
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            />
             <label htmlFor="content">Project Content</label>
           </div>
           <div className="input-field">
@@ -39,20 +44,23 @@ class CreateProject extends Component {
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     auth: state.firebase.auth
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
+    createProject: project => dispatch(createProject(project))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateProject);
